152df8aa86710bfd4a7a81edf07a3574b8124d54
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2011-11-06 00:30:44 +0100

    softpll.h: added defs and prototypes for polling aux clock status

--- include/softpll.h
+++ include/softpll.h
@@ -1,12 +1,16 @@
 #ifndef __SOFTPLL_H
 #define __SOFTPLL_H
 
+#define SOFTPLL_AUX_ENABLED 1
+#define SOFTPLL_AUX_LOCKED 2
+
 void softpll_enable();
 int softpll_check_lock();
 void softpll_disable();
 int softpll_busy();
 void softpll_set_phase(int ps);
 int softpll_get_setpoint();
+int softpll_get_aux_status();
 
 #endif
 
