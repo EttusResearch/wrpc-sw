53d94c0af43913cb121a90201fcd95231e36fdb8
Grzegorz Daniluk <g.daniluk@elproma.com.pl>
2012-02-08 23:50:29 +0100

    wrcore_v2: modified memory map, less bits used, for wr-nic

--- include/board.h
+++ include/board.h
@@ -1,13 +1,13 @@
 #ifndef __BOARD_H
 #define __BOARD_H
 
-#define BASE_MINIC    0x40000
-#define BASE_EP   	  0x40100 
-#define BASE_SOFTPLL  0x40200 
-#define BASE_PPSGEN   0x40300
-#define BASE_SYSCON   0x40400
-#define BASE_UART 	  0x40500
-#define BASE_ONEWIRE  0x40600
+#define BASE_MINIC    0x20000
+#define BASE_EP   	  0x20100 
+#define BASE_SOFTPLL  0x20200 
+#define BASE_PPSGEN   0x20300
+#define BASE_SYSCON   0x20400
+#define BASE_UART 	  0x20500
+#define BASE_ONEWIRE  0x20600
 
 #define CPU_CLOCK 62500000ULL
 
