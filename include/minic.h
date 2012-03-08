0b36aae535ec0a9b4b5603c9af45dc559b273414
Grzegorz Daniluk <g.daniluk@elproma.com.pl>
2012-03-09 00:03:09 +0100

    include/minic: add missing function header

--- include/minic.h
+++ include/minic.h
@@ -8,6 +8,7 @@
 void minic_init();
 void minic_disable();
 int minic_poll_rx();
+void minic_get_stats(int *tx_frames, int *rx_frames);
 
 int minic_rx_frame(uint8_t *hdr, uint8_t *payload, uint32_t buf_size, struct hw_timestamp *hwts);
 int minic_tx_frame(uint8_t *hdr, uint8_t *payload, uint32_t size, struct hw_timestamp *hwts);
