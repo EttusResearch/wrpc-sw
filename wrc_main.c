375d1b40289ee7bc3ec3a1e931f92524689371cf
Grzegorz Daniluk <grzegorz.daniluk@cern.ch>
2012-08-02 16:53:04 +0200

    wrc_main: default deltaRx corrected (did not took into account oscilloscope cables delay)

--- wrc_main.c
+++ wrc_main.c
@@ -25,7 +25,7 @@ int wrc_ui_mode = UI_SHELL_MODE;
 //Calibration data (from EEPROM if available)
 int32_t sfp_alpha = -73622176;  //default value if could not read EEPROM
 int32_t sfp_deltaTx = 46407;  //default value if could not read EEPROM
-int32_t sfp_deltaRx = 189093; //default value if could not read EEPROM
+int32_t sfp_deltaRx = 177093; //default value if could not read EEPROM
 uint32_t cal_phase_transition = 2394; //7000;
 
 void wrc_initialize()
