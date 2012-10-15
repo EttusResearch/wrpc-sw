ab30804f7ab602947bef552e8fde1087507707f8
Alessandro Rubini <rubini@gnudd.com>
2012-10-15 08:25:02 +0200

    kconfig: use 'Kconfig' as file name, not the bst-kconfig ones
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

--- scripts/kconfig/confdata.c
+++ scripts/kconfig/confdata.c
@@ -21,7 +21,7 @@ static void conf_warning(const char *fmt, ...)
 static const char *conf_filename;
 static int conf_lineno, conf_warnings, conf_unsaved;
 
-const char conf_defname[] = "fm/main.fm";
+const char conf_defname[] = "Kconfig";
 
 static void conf_warning(const char *fmt, ...)
 {
