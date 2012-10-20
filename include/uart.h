ff1ba05213235ab5e8b35a7c7d38b09bdf0b2a5a
Alessandro Rubini <rubini@gnudd.com>
2012-10-20 09:51:31 +0200

    include: add __attribute__((format(printf))) to mprintf
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

--- include/uart.h
+++ include/uart.h
@@ -1,7 +1,8 @@
 #ifndef __UART_H
 #define __UART_H
 
-int mprintf(char const *format, ...);
+int mprintf(char const *format, ...)
+	__attribute__((format(printf,1,2)));
 
 void uart_init();
 void uart_write_byte(int b);
