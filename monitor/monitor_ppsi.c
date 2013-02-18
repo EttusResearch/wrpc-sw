a0d1be1c9a9c50ba38045b7156a8389da881d1fa
Aurelio Colosimo <aurelio@aureliocolosimo.it>
2013-02-18 23:56:52 +0100

    monitor/monitor_ppsi.c: currently empty
    This is a placeholder for ppsi, filled by later patches
    
    Signed-off-by: Aurelio Colosimo <aurelio@aureliocolosimo.it>
    Acked-by: Alessandro Rubini <rubini@gnudd.com>

---
+++ monitor/monitor_ppsi.c
@@ -0,0 +1,21 @@
+/*
+ * This work is part of the White Rabbit project
+ *
+ * Copyright (C) 2011,2012 CERN (www.cern.ch)
+ * Author: Aurelio Colosimo <aurelio@aureliocolosimo.it>
+ *
+ * Released according to the GNU GPL, version 2 or any later version.
+ */
+
+#include <inttypes.h>
+
+void wrc_mon_gui(void)
+{
+	/* FIXME stub: everything to be done, see monitor.c */
+}
+
+int wrc_log_stats(uint8_t onetime)
+{
+	/* FIXME stub: everything to be done, see monitor.c */
+	return 0;
+}
