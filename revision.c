31c79183d5eacca80d75226166ca5b2cbb1db54d
Alessandro Rubini <rubini@gnudd.com>
2013-05-07 10:59:05 +0200

    cmd_version: better output, report unsupported builds
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

--- revision.c
+++ revision.c
@@ -4,6 +4,6 @@ const char *build_revision = GIT_REVISION;
 #ifdef CONFIG_DETERMINISTIC_BINARY
 const char *build_date = "";
 #else
-const char *build_date = ", compiled on: " __DATE__ " " __TIME__;
+const char *build_date = "Built on " __DATE__ ", " __TIME__ "\n";
 #endif
 
--- shell/cmd_version.c
+++ shell/cmd_version.c
@@ -5,10 +5,25 @@
 
 extern const char *build_revision, *build_date;
 
+#ifdef CONFIG_DEVELOPER
+#define SUPPORT " (unsupported developer build)"
+#else
+#define SUPPORT ""
+#endif
+
 static int cmd_ver(const char *args[])
 {
-	mprintf("WR Core build: %s%s (memory size: %d kB)\n",
-		build_revision, build_date, sysc_get_memsize());
+	int hwram = sysc_get_memsize();
+
+	pp_printf("WR Core build: %s%s\n", build_revision, SUPPORT);
+	pp_printf("%s", build_date); /* may be empty, or complete with \n */
+	pp_printf("Built for %d kB RAM, stack is %d bytes\n",
+		  CONFIG_RAMSIZE / 1024, CONFIG_STACKSIZE);
+	/* hardware reports memory size, with a 16kB granularity */
+	if ( hwram / 16 != CONFIG_RAMSIZE / 1024 / 16)
+		pp_printf("WARNING: hardware says %ikB <= RAM < %ikB\n",
+			  hwram, hwram + 16);
+
 	return 0;
 }
 
