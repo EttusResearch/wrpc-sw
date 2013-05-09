7ff210e55258d7293ef7a582cc3225efc815cb28
Alessandro Rubini <rubini@gnudd.com>
2013-05-09 14:06:24 +0200

    for-ppsi: shell/cmd_verbose: use new diagnostic mask
    Actually, I tried to move cmd_verbose.c to ppsi, and avoid
    this dependency, but our use of --gc-sections would prevent
    the code to be picked into wrc.o.
    
    Hopefully, we won't need to touch this command any more.
    
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

--- shell/cmd_verbose.c
+++ shell/cmd_verbose.c
@@ -1,29 +1,21 @@
 /*
  * This work is part of the White Rabbit project
  *
- * Copyright (C) 2012 GSI (www.gsi.de)
- * Author: Wesley W. Terpstra <w.terpstra@gsi.de>
- *
  * Released according to the GNU GPL, version 2 or any later version.
  */
 
 #include <wrc.h>
 #include <shell.h>
-
-extern int pp_diag_verbosity;
+#include <ppsi/ppsi.h>
 
 static int cmd_verbose(const char *args[])
 {
 	int v;
 	v = args[0][0] - '0';
 
-	if (v < 0)
-		v = 0;
-	else if (v > 2)
-		v = 2;
-
-	pp_printf("PPSI verbosity set to %d\n", v);
-	pp_diag_verbosity = v;
+	if (args[0])
+		pp_global_flags = pp_diag_parse((char *)args[0]);
+	pp_printf("PPSI verbosity: %08lx\n", pp_global_flags);
 	return 0;
 }
 
