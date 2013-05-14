535fe62fa01fa8b66de717c4a5987d1fc3a54ed9
Alessandro Rubini <rubini@gnudd.com>
2013-05-14 18:56:52 +0200

    add sdbfs support in eeprom (W1 only by now)
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

--- dev/dev.mk
+++ dev/dev.mk
@@ -1,5 +1,4 @@
 obj-y += \
-	dev/eeprom.o \
 	dev/endpoint.o \
 	dev/ep_pfilter.o \
 	dev/i2c.o \
@@ -9,6 +8,9 @@ obj-y += \
 	dev/sfp.o \
 	dev/sdb.o \
 	dev/rxts_calibrator.o
+
+obj-$(CONFIG_LEGACY_EEPROM) += dev/eeprom.o
+obj-$(CONFIG_SDB_EEPROM) += dev/sdb-eeprom.o
 
 obj-$(CONFIG_SOCKITOWM) +=	dev/onewire.o
 obj-$(CONFIG_W1) +=		dev/w1.o	dev/w1-hw.o
