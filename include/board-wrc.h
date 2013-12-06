5f5800c1526bb1efceaf761175b2edadd340ab06
Alessandro Rubini <rubini@gnudd.com>
2013-12-06 11:32:45 +0100

    softpll-unify: split board.h in two headers, for wrc and wrs
    This has no effect on wrc.bin: board.h is renamed into board-wrc.h
    and included by the new board.h. The new board-wrs.h is copied from
    wr-switch-sw/rt/include/board.h
    
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

---
+++ include/board-wrc.h
@@ -0,0 +1,34 @@
+#ifndef __BOARD_WRC_H
+#define __BOARD_WRC_H
+/*
+ * This is meant to be automatically included by the Makefile,
+ * when wrpc-sw is build for wrc (node) -- as opposed to wrs (switch)
+ */
+
+#include <hw/memlayout.h>
+
+/* Board-specific parameters */
+
+/* WR Core system/CPU clock frequency in Hz */
+#define CPU_CLOCK 62500000ULL
+
+/* WR Reference clock period (picoseconds) and frequency (Hz) */
+#define REF_CLOCK_PERIOD_PS 8000
+#define REF_CLOCK_FREQ_HZ 125000000
+
+/* Baud rate of the builtin UART (does not apply to the VUART) */
+#define UART_BAUDRATE 115200ULL
+
+/* Maximum number of simultaneously created sockets */
+#define NET_MAX_SOCKETS 4
+
+/* Socket buffer size, determines the max. RX packet size */
+#define NET_SKBUF_SIZE 512
+
+/* Number of auxillary clock channels - usually equal to the number of FMCs */
+#define NUM_AUX_CLOCKS 1
+
+int board_init();
+int board_update();
+
+#endif /* __BOARD_WRC_H */
