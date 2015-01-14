8f6a011e40cb0deb21b03b141e6b3efe370a2e1c
Adam Wujek <adam.wujek@cern.ch>
2015-01-14 11:33:21 +0100

    libwr: add hal_shmem.h and sfp_lib.h
    Files needed to get rid of halexp_get_port_state.
    Done during process of moving HAL to shm in wrs.
    
    Signed-off-by: Adam Wujek <adam.wujek@cern.ch>

---
+++ include/libwr/hal_shmem.h
@@ -0,0 +1,142 @@
+#ifndef __LIBWR_HAL_SHMEM_H__
+#define __LIBWR_HAL_SHMEM_H__
+
+#include <hal_exports.h>
+#include <libwr/sfp_lib.h>
+#include <string.h>
+
+/* Port state machine states */
+#define HAL_PORT_STATE_DISABLED 0
+#define HAL_PORT_STATE_LINK_DOWN 1
+#define HAL_PORT_STATE_UP 2
+#define HAL_PORT_STATE_CALIBRATION 3
+#define HAL_PORT_STATE_LOCKING 4
+
+#define DEFAULT_T2_PHASE_TRANS 0
+#define DEFAULT_T4_PHASE_TRANS 0
+
+/* Port delay calibration parameters */
+typedef struct hal_port_calibration {
+
+	/* PHY delay measurement parameters for PHYs which require
+	   external calibration (i.e. with the feedback network. */
+
+	/* minimum possible delay introduced by the PHY. Expressed as time
+	   (in picoseconds) between the beginning of the symbol on the serial input
+	   and the rising edge of the RX clock at which the deserialized word is
+	   available at the parallel output of the PHY. */
+	uint32_t phy_rx_min;
+
+	/* the same set of parameters, but for the TX path of the PHY */
+	uint32_t phy_tx_min;
+
+	/* Current PHY (clock-to-serial-symbol) TX and RX delays, in ps */
+	uint32_t delta_tx_phy;
+	uint32_t delta_rx_phy;
+
+	/* Current board routing delays (between the DDMTD inputs to
+	   the PHY clock inputs/outputs), in picoseconds */
+	uint32_t delta_tx_board;
+	uint32_t delta_rx_board;
+
+	/* When non-zero: RX path is calibrated (delta_*_rx contain valid values) */
+	int rx_calibrated;
+	/* When non-zero: TX path is calibrated */
+	int tx_calibrated;
+
+	struct shw_sfp_caldata sfp;
+
+} hal_port_calibration_t;
+
+/* Internal port state structure */
+struct hal_port_state {
+	/* non-zero: allocated */
+	int in_use;
+	/* linux i/f name */
+	char name[16];
+
+	/* MAC addr */
+	uint8_t hw_addr[6];
+
+	/* ioctl() hw index */
+	int hw_index;
+
+	/* file descriptor for ioctls() */
+	int fd;
+	int hw_addr_auto;
+
+	/* port timing mode (HEXP_PORT_MODE_xxxx) */
+	int mode;
+
+	/* port FSM state (HAL_PORT_STATE_xxxx) */
+	int state;
+
+	/* fiber type, used to get alpha for SFP frequency */
+	int fiber_index;
+
+	/* 1: PLL is locked to this port */
+	int locked;
+
+	/* calibration data */
+	hal_port_calibration_t calib;
+
+	/* current DMTD loopback phase (ps) and whether is it valid or not */
+	uint32_t phase_val;
+	int phase_val_valid;
+	int tx_cal_pending, rx_cal_pending;
+	/* locking FSM state */
+	int lock_state;
+
+	/*reference lock period in picoseconds*/
+	uint32_t clock_period;
+
+	/* approximate DMTD phase value (on slave port) at which RX timestamp
+	 * (T2) counter transistion occurs (picoseconds) */
+	uint32_t t2_phase_transition;
+
+	/* approximate phase value (on master port) at which RX timestamp (T4)
+	 * counter transistion occurs (picoseconds) */
+	uint32_t t4_phase_transition;
+
+	/* Endpoint's base address */
+	uint32_t ep_base;
+};
+
+/* This is the overall structure stored in shared memory */
+#define HAL_SHMEM_VERSION 3 /* Version 3 because sfp delta is signed */
+struct hal_shmem_header {
+	int nports;
+	struct hal_port_state *ports;
+};
+
+/*
+ * The following functions were in userspace/wrsw_hal/hal_ports.c,
+ * and are used to marshall data for the RPC format. Now that we
+ * offer shared memory, it is the caller who must convert data to
+ * the expected format (which remains the RPC one as I write this).
+ */
+struct hal_port_state *hal_port_lookup(struct hal_port_state *ports,
+				       const char *name);
+
+int hal_port_get_exported_state(struct hexp_port_state *state,
+				struct hal_port_state *ports,
+				const char *port_name);
+
+static inline int state_up(int state)
+{
+	return (state != HAL_PORT_STATE_LINK_DOWN
+		     && state != HAL_PORT_STATE_DISABLED);
+}
+
+static inline struct hal_port_state *hal_lookup_port(
+			struct hal_port_state *ports, int nports, char *name)
+{
+	int i;
+
+	for (i = 0; i < nports; i++)
+		if (ports[i].in_use && (!strcmp(name, ports[i].name)))
+			return ports + i;
+	return NULL;
+}
+
+#endif /*  __LIBWR_HAL_SHMEM_H__ */
---
+++ include/libwr/sfp_lib.h
@@ -0,0 +1,111 @@
+#ifndef __LIBWR_SHW_SFPLIB_H
+#define __LIBWR_SHW_SFPLIB_H
+
+#define SFP_LED_LINK	(1 << 0)
+#define SFP_LED_WRMODE	(1 << 1)
+#define SFP_LED_SYNCED	(1 << 2)
+#define SFP_TX_DISABLE	(1 << 3)
+
+#define shw_sfp_set_led_link(num, status)	\
+	shw_sfp_set_generic(num, status, SFP_LED_LINK)
+
+#define shw_sfp_set_led_wrmode(num, status)	\
+	shw_sfp_set_generic(num, status, SFP_LED_WRMODE)
+
+#define shw_sfp_set_led_synced(num, status)	\
+	shw_sfp_set_generic(num, status, SFP_LED_SYNCED)
+
+#define shw_sfp_set_tx_disable(num, status)	\
+	shw_sfp_set_generic(num, status, SFP_TX_DISABLE)
+
+#define SFP_FLAG_CLASS_DATA	(1 << 0)
+#define SFP_FLAG_DEVICE_DATA	(1 << 1)
+
+struct shw_sfp_caldata {
+	int flags;
+	/*
+	 * Part number used to identify it. Serial number because we
+	 * may specify per-specimen delays, but it is not used at this
+	 * point in time
+	 */
+	char part_num[16];
+	char vendor_serial[16];
+	/* Callibration data */
+	double alpha;
+	int delta_tx_ps; /* "delta" of this SFP type WRT calibration type */
+	int delta_rx_ps;
+	/* wavelengths, used to get alpha from fiber type */
+	int tx_wl;
+	int rx_wl;
+	/* and link as a list */
+	struct shw_sfp_caldata *next;
+};
+
+struct shw_sfp_header {
+	uint8_t id;
+	uint8_t ext_id;
+	uint8_t connector;
+	uint8_t transciever[8];
+	uint8_t encoding;
+	uint8_t br_nom;
+	uint8_t reserved1;
+	uint8_t length1;	/* Link length supported for 9/125 mm fiber (km) */
+	uint8_t length2;	/* Link length supported for 9/125 mm fiber (100m) */
+	uint8_t length3;	/* Link length supported for 50/125 mm fiber (10m) */
+	uint8_t length4;	/* Link length supported for 62.5/125 mm fiber (10m) */
+	uint8_t length5;	/* Link length supported for copper (1m) */
+	uint8_t reserved2;
+	uint8_t vendor_name[16];
+	uint8_t reserved3;
+	uint8_t vendor_oui[3];
+	uint8_t vendor_pn[16];
+	uint8_t vendor_rev[4];
+	uint8_t reserved4[3];
+	uint8_t cc_base;
+
+	/* extended ID fields start here */
+	uint8_t options[2];
+	uint8_t br_max;
+	uint8_t br_min;
+	uint8_t vendor_serial[16];
+	uint8_t date_code[8];
+	uint8_t reserved[3];
+	uint8_t cc_ext;
+} __attribute__ ((packed));
+
+/* Public API */
+
+/*
+ * Scan all ports for plugged in SFP's. The return value is a bitmask
+ * of all the ports with detected SFP's (bits 0-17 are valid).
+ */
+uint32_t shw_sfp_module_scan(void);
+
+/* Set/get the 4 GPIO's connected to PCA9554's for a particular SFP */
+void shw_sfp_gpio_set(int num, uint8_t state);
+uint8_t shw_sfp_gpio_get(int num);
+
+static inline void shw_sfp_set_generic(int num, int status, int type)
+{
+	uint8_t state;
+	state = shw_sfp_gpio_get(num);
+	if (status)
+		state |= type;
+	else
+		state &= ~type;
+	shw_sfp_gpio_set(num, state);
+}
+
+/* Load the db from dot-config to internal structures */
+int shw_sfp_read_db(void);
+
+/* Read and verify the header all at once. returns -1 on failure */
+int shw_sfp_read_verify_header(int num, struct shw_sfp_header *head);
+
+/* return NULL if no data found */
+struct shw_sfp_caldata *shw_sfp_get_cal_data(int num);
+
+/* Read and verify the header all at once. returns -1 on failure */
+int shw_sfp_read_verify_header(int num, struct shw_sfp_header *head);
+
+#endif /* __LIBWR_SHW_SFPLIB_H */
