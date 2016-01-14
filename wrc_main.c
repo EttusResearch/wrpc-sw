ce414c802b3b074b680a3521f355d00805a50ee6
Alessandro Rubini <rubini@gnudd.com>
2016-01-14 23:39:18 +0100

    wrc_main: call ipv4_poll at link-down event, so it knows
    This is needed for the syslog code to know about link up and down
    events, to report a link-up with the delay since last link-down.
    
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

--- wrc_main.c
+++ wrc_main.c
@@ -179,19 +179,18 @@ int main(void)
 		}
 
 		switch (l_status) {
+		case LINK_WENT_DOWN:
+			if (wrc_ptp_get_mode() == WRC_MODE_SLAVE) {
+				spll_init(SPLL_MODE_FREE_RUNNING_MASTER, 0, 1);
+				shw_pps_gen_enable_output(0);
+			}
+			/* fall through */
 		case LINK_WENT_UP:
 		case LINK_UP:
 			update_rx_queues();
 			if (HAS_IP) {
 				ipv4_poll(l_status);
 				arp_poll();
-			}
-			break;
-
-		case LINK_WENT_DOWN:
-			if (wrc_ptp_get_mode() == WRC_MODE_SLAVE) {
-				spll_init(SPLL_MODE_FREE_RUNNING_MASTER, 0, 1);
-				shw_pps_gen_enable_output(0);
 			}
 			break;
 		}
