a6a7a489d6f3603bcecc0e687e7a5a1efdaac6c8
Alessandro Rubini <rubini@gnudd.com>
2016-04-04 13:15:00 +0200

    lib: do not build IPV4 code if not configured in
    Although we love to build-test stuff and discard at link time, as soon
    as we move tasks to be sections, we need to avoid building the file of
    the task if not configured in.  This means lib/arp.c and lib/ipv4.c.
    
    But, for consistency, avoid building icmp.c and bootp.c as well.
    The alternative, which is ifconfig'ing the task itself results in
    warnings like "'ipv4_init' defined but not used".
    
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

--- lib/lib.mk
+++ lib/lib.mk
@@ -4,8 +4,7 @@ obj-$(CONFIG_LM32) += \
 	lib/atoi.o \
 	lib/usleep.o
 
-obj-$(CONFIG_WR_NODE) += lib/net.o lib/udp.o
+obj-$(CONFIG_WR_NODE) += lib/net.o
 
-obj-$(CONFIG_WR_NODE) += lib/arp.o lib/icmp.o lib/ipv4.o lib/bootp.o
-
+obj-$(CONFIG_IP) += lib/ipv4.o lib/arp.o lib/icmp.o lib/udp.o lib/bootp.o
 obj-$(CONFIG_SYSLOG) += lib/syslog.o
