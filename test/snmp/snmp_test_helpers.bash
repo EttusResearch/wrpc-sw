388baa39d56fef4c5a7f391e0771cbb2746637f3
Adam Wujek <adam.wujek@cern.ch>
2016-07-15 17:47:44 +0200

    test/snmp: add tests for aux registers
    Signed-off-by: Adam Wujek <adam.wujek@cern.ch>

--- test/snmp/snmp_test_helpers.bash
+++ test/snmp/snmp_test_helpers.bash
@@ -4,12 +4,12 @@ helper_snmpget() {
 }
 
 helper_snmpget_oidnotfound() {
-  run snmpget $SNMP_OPTIONS $TARGET_IP $1
+  run snmpget $SNMP_OPTIONS_NO_M $TARGET_IP $1
   [ "$status" -eq 2 ]
 }
 
 helper_snmpget_oidfound() {
-  result="$(snmpget $SNMP_OPTIONS $TARGET_IP $1 | grep "$1" | wc -l)"
+  result="$(snmpget $SNMP_OPTIONS_NO_M $TARGET_IP $1 | grep "$1" | wc -l)"
   [ "$result" -eq 1 ]
 }
 
@@ -23,6 +23,17 @@ helper_snmpset() {
   result="$(snmpset $SNMP_OPTIONS $TARGET_IP $1 = "$2" | grep "$2" | wc -l)"
   [ "$result" -eq 1 ]
 }
+
+helper_snmpset_u() {
+  result="$(snmpset $SNMP_OPTIONS $TARGET_IP $1 u "$2" | grep "$2" | wc -l)"
+  [ "$result" -eq 1 ]
+}
+
+helper_snmpset_failed_u() {
+  run snmpset $SNMP_OPTIONS $TARGET_IP $1 u "$2"
+  [ "$status" -eq 2 ]
+}
+
 
 helper_erase_sfp_database() {
   result="$(snmpset $SNMP_OPTIONS $TARGET_IP wrpcPtpConfigApply.0 = eraseFlash | grep applySuccessful | wc -l)"
