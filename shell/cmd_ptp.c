e743b7f625c702a16ff24ea132239425059120b9
Alessandro Rubini <rubini@gnudd.com>
2016-10-27 11:43:03 +0200

    shell/cmd_ptp: be able to set mechanism
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

--- shell/cmd_ptp.c
+++ shell/cmd_ptp.c
@@ -17,6 +17,10 @@ struct subcmd {
 } subcmd[] = {
 	{"start", wrc_ptp_run, 1},
 	{"stop", wrc_ptp_run, 0},
+	{"e2e", wrc_ptp_sync_mech, PP_E2E_MECH},
+	{"delay", wrc_ptp_sync_mech, PP_E2E_MECH},
+	{"p2p", wrc_ptp_sync_mech, PP_P2P_MECH},
+	{"pdelay", wrc_ptp_sync_mech, PP_P2P_MECH},
 };
 
 static int cmd_ptp(const char *args[])
