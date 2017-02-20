e882dca2340d6b77503c9415aab8af53ef090c0c
Adam Wujek <adam.wujek@cern.ch>
2017-02-20 15:57:18 +0100

    save_size.sh: don't printout when size_info_file not specified
    Signed-off-by: Adam Wujek <adam.wujek@cern.ch>

--- save_size.sh
+++ save_size.sh
@@ -3,11 +3,9 @@
 set -e
 
 if ! [ -n "$size_info_file" ]; then
-    echo "No file with size info specified!"
     exit 0
 fi
 
-echo $1 $2 $DEFCONFIG_NAME
 # separate calling commands and filling DB
 SIZES=`$1 $2 | grep $2`
 GIT_HASH=`git log --format=format:%H -1`
